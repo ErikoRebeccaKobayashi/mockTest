68e1bac17238b537b1afcd60321ab8be7b390c91
Kylie Petitt <kylieppetitt@gmail.com>
2019-05-01 20:04:17 +0100

    added html form

--- index.php
+++ index.php
@@ -7,11 +7,80 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <!--bootstrap-->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+        <title>James Bond Programme</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <h1>James Bond film programme</h1>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Channel</th>
+                    <th scope="col">9am</th>
+                    <th scope="col">10am</th>
+                    <th scope="col">11am</th>
+                    <th scope="col">12pm</th>
+                    <th scope="col">1pm</th>
+                    <th scope="col">2pm</th>
+                    <th scope="col">3pm</th>
+                    <th scope="col">4pm</th>
+                    <th scope="col">5pm</th>
+                    <th scope="col">6pm</th>
+                    <th scope="col">7pm</th>
+                    <th scope="col">8pm</th>
+                    <th scope="col">9pm</th>
+                    <th scope="col">10pm</th>
+                    <th scope="col">11pm</th>
+                    <th scope="col">12am</th>
+                </tr>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Sean Connery</th>
+                    <td colspan="8">No programme</td>
+                    <td colspan="4">No programme</td>
+                    <td colspan="4">No programme</td>
+                </tr>
+                <tr>
+                    <th scope="row">Roger Moore</th>
+                    <td colspan="8">No programme</td>
+                    <td colspan="4">No programme</td>
+                    <td colspan="4">No programme</td>
+                </tr>
+                <tr>
+                    <th scope="row">Timothy Dalton</th>
+                    <td colspan="8">No programme</td>
+                    <td colspan="4">No programme</td>
+                    <td colspan="4">No programme</td>
+                </tr>
+                <tr>
+                    <th scope="row">George Lazenby</th>
+                    <td colspan="8">No programme</td>
+                    <td colspan="4">No programme</td>
+                    <td colspan="4">No programme</td>
+                </tr>
+                <tr>
+                    <th scope="row">Pierce Brosnan</th>
+                    <td colspan="8">No programme</td>
+                    <td colspan="4">No programme</td>
+                    <td colspan="4">No programme</td>
+                </tr>
+                <tr>
+                    <th scope="row">Daniel Craig</th>
+                    <td colspan="10">No programme</td>
+                    <td colspan="2">No programme</td>
+                    <td colspan="3">No programme</td>
+                </tr>
+            </tbody>
+        </table>
+        <!--javascript-->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
     </body>
 </html>
